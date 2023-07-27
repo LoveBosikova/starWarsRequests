09609d8a7778f4222cee3cf9385c0103802fa8a0
Bosikova Liubov Sergeevna <gulidova06@mail.ru>
2023-07-27 16:40:10 +0300

    request done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const form = document.querySelector('form');
 console.log(form.elements.number);
 
+// Проверка валидности введённых данных
 form.elements.number.addEventListener('keyup', () => {
     if (form.elements.number.value > 10) {
         document.querySelector('.numErr').textContent = 'Умерь запрос до 10';
@@ -17,20 +18,30 @@ form.elements.searchObject.addEventListener('change', () => {
     }
 });
 
-form.addEventListener('submit', (e) => {
-   e.preventDefault();
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
     if (form.elements.searchObject.value == 'failure') {
         document.querySelector('.valErr').textContent = 'Пустой запрос не ведёт к свету. Сделай выбор';
     } else {
         document.querySelector('.valErr').textContent = '';
     }
 
-    console.log(form.elements.number.value);
-
     if (form.elements.number.value == '') {
         document.querySelector('.numErr').textContent = 'Пустота порождает пустоту. Введи число';
     } else {
         document.querySelector('.numErr').textContent = '';
     }
+    try{
+        const response = await fetch(createQuery());
+    } catch (err) {
+        
+    }
+    console.log(response);
+    const item = await response.json();
+    console.log(item);
+});
 
- });
+//Формируем запрос 
+function createQuery(){
+    return `https://swapi.dev/api/${form.elements.searchObject.value}/${form.elements.number.value}/`;
+}
